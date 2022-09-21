75a54c9af4786b26dc9f4f5dd065fb0a180b2bc1
Philip Turner <philipturner.AR@gmail.com>
2022-09-20 21:16:11 -0400

    Update RenderUtilsGraph_Changes.cpp

--- Sources/RenderUtilsGraph_Changes.cpp
+++ Sources/RenderUtilsGraph_Changes.cpp
@@ -1,9 +1,9 @@
 static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
 {
-  int32 ResourceType = 1; //RHIGetPreferredClearUAVRectPSResourceType(Parameters.Platform);
+	int32 ResourceType = 1; //RHIGetPreferredClearUAVRectPSResourceType(Parameters.Platform);
 
-  FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
-  OutEnvironment.SetDefine(TEXT("ENABLE_CLEAR_VALUE"), 1);
-  OutEnvironment.SetDefine(TEXT("RESOURCE_TYPE"), ResourceType);
-  OutEnvironment.SetDefine(TEXT("VALUE_TYPE"), TEXT("uint4"));
+	FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
+	OutEnvironment.SetDefine(TEXT("ENABLE_CLEAR_VALUE"), 1);
+	OutEnvironment.SetDefine(TEXT("RESOURCE_TYPE"), ResourceType);
+	OutEnvironment.SetDefine(TEXT("VALUE_TYPE"), TEXT("uint4"));
 }
